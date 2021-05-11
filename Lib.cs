724738ec52e2f0250934ba4b53fcd64e1b356813
vivek <vivek@athenaeum.in>
2021-05-11 10:45:01 +0530

    Add project files.

---
+++ Lib.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Behavioural.TemplateMethod.V2
+{
+    public abstract class OrderBase
+    {
+        //  High-level steps
+        protected abstract void EnsurePayment();
+        protected abstract void CheckStock();
+        protected abstract void PackItems();
+        protected abstract void ShipItems();
+
+        //  template-method
+        public void Process()
+        {
+            EnsurePayment();
+            CheckStock();
+            PackItems();
+            ShipItems();
+        }
+    }
+
+
+    public class PremiumOrder : OrderBase
+    {
+        protected override void CheckStock()
+        {
+            Console.WriteLine("PremiumOrder: CheckStock");
+        }
+
+        protected override void EnsurePayment()
+        {
+            Console.WriteLine("PremiumOrder: EnsurePayment");
+        }
+
+        protected override void PackItems()
+        {
+            Console.WriteLine("PremiumOrder: PackItems");
+        }
+
+        protected override void ShipItems()
+        {
+            Console.WriteLine("PremiumOrder: ShipItems");
+        }
+    }
+
+    public class RegularOrder : OrderBase
+    {
+        protected override void CheckStock()
+        {
+            Console.WriteLine("RegularOrder: CheckStock");
+        }
+
+        protected override void EnsurePayment()
+        {
+            Console.WriteLine("RegularOrder: EnsurePayment");
+        }
+
+        protected override void PackItems()
+        {
+            Console.WriteLine("RegularOrder: PackItems");
+        }
+
+        protected override void ShipItems()
+        {
+            Console.WriteLine("RegularOrder: ShipItems");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Behavioural.TemplateMethod.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderBase regularOrder = new RegularOrder();
+            OrderBase premiumOrder = new PremiumOrder();
+
+            regularOrder.Process(); //  template method
+            Console.WriteLine("-------------------------");
+            premiumOrder.Process();
+        }
+    }
+}
